5eef145cb7db413d10a303b5dde44aa1ed75ed76
Abdulmunim <amunim729@gmail.com>
2023-12-09 01:40:22 +0300

    Added NGram Modeling Algorithm

---
+++ src/ngram_model.py
@@ -0,0 +1,34 @@
+from utils.amharic_tokenizer import AmharicSegmenter
+
+# ngram_model.py
+
+def generate_ngrams(tokens: str, n: int) -> list:
+    """
+    Generate a list of n-grams from the given text.
+    """
+    ngrams = []
+    for i in range(len(tokens) - n + 1):
+        ngram = text[i:i+n]
+        ngrams.append(ngram)
+    return ngrams
+
+
+# ngram_model.py
+
+def build_ngram_model(corpus: list, n: int) -> dict:
+    """
+    Build an N-gram model from the given list of corpus.
+    """
+    ngram_model = {}
+    tokenizer = AmharicSegmenter()
+    if type(corpus) == str:
+        corpus = tokenizer.tokenize_sentence(corpus)
+    for sentence in corpus:
+        sentence = tokenizer.tokenize(sentence)
+        ngrams = generate_ngrams(sentence, n)
+        for ngram in ngrams:
+            if ngram in ngram_model:
+                ngram_model[ngram] += 1
+            else:
+                ngram_model[ngram] = 1
+    return ngram_model
