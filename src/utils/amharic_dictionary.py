fd142d147805f6f735dbb42cc8c6ee6a0e5079c7
Abdulmunim <amunim729@gmail.com>
2023-12-09 10:32:36 +0300

    Implemented Encapsulation Principle

--- src/utils/amharic_dictionary.py
+++ src/utils/amharic_dictionary.py
@@ -4,18 +4,17 @@ from collections import defaultdict
 class Dictionary:
     def __init__(self, dictionary_path):
         self.dictionary_path = dictionary_path
-        self.dictionary = defaultdict(lambda: 0)
-        self._dictionary_words = self.dictionary.keys()
-        
+        self._dictionary = defaultdict(lambda: 0)
         dictionary = read_lines(self.dictionary_path)
         for word in dictionary:
-            self.dictionary[word] += 1
+            self._dictionary[word] += 1
+        self._dictionary_words = self._dictionary.keys()
     
     def __contains__(self, word):
-        return self.dictionary[word] != 0
+        return self._dictionary[word] != 0
     
     def look(self, word):
-        return self.dictionary[word] != 0
+        return self._dictionary[word] != 0
     
     def get_words(self):
         return self._dictionary.keys()
