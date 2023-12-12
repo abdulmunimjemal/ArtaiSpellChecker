18999e982f7438758637de2886f01b53ce622ab6
Abdulmunim <amunim729@gmail.com>
2023-12-12 11:43:18 +0300

    Reorganized in classes

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,28 +1,30 @@
 import re
 from utils.amharic_normalizer import AmharicNormalizer
 
-def remove_non_amharic(text: str) -> str:
-    """
-    Remove non-Amharic characters from the text.
-    """
-    amharic_pattern = re.compile('[ሀ-፼ ]+')
-    matches = amharic_pattern.findall(text)
-    cleaned_text = ''.join(matches)
-    return cleaned_text
-   
-def normalize(text: str) -> str:
-    """
-    Normalize Amharic Text
-    """
-    normalizer = AmharicNormalizer()
-    return normalizer.normalize(text)
-    
+class AmharicTextProcessor:
+    def __init__(self):
+        self.normalizer = AmharicNormalizer()
+        self.amharic_pattern = re.compile('[ሀ-፼ ]+')
 
-def preprocess(text: str) -> str:
-    """
-    Perform general preprocessing steps on the input text.
-    """
-    text = normalize(text)
-    text = remove_non_amharic(text)
-    
-    return text
+    def remove_non_amharic(self, text: str) -> str:
+        """
+        Remove non-Amharic characters from the text.
+        """
+        matches = self.amharic_pattern.findall(text)
+        cleaned_text = ''.join(matches)
+        return cleaned_text
+
+    def normalize(self, text: str) -> str:
+        """
+        Normalize Amharic Text
+        """
+        return self.normalizer.normalize(text)
+
+    def preprocess(self, text: str) -> str:
+        """
+        Perform general preprocessing steps on the input text.
+        """
+        text = self.normalize(text)
+        text = self.remove_non_amharic(text)
+        return text
+
